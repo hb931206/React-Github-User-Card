454a78b06a5152752950d1584922cdcebe1c32d6
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-04-10 19:59:32 -0400

    Class App component setup.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Henry</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
