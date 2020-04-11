e4744fdb1209643407be176d2c9bda7f332d9d61
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-04-10 20:35:24 -0400

    Fetched my data.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ import "./App.css";
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      henry: {},
+    };
   }
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/hb931206")
+      .then((res) => res.json())
+      .then((user) => {
+        console.log("Me:", user);
+        this.setState({ henry: user });
+        console.log(this.state.henry);
+      })
+      .catch((err) => {
+        console.log("Err:", err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
