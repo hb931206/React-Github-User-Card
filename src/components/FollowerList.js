fdf3e8a23459f8b1e7ad9ef702ebefc3c971e6e4
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-04-12 14:08:04 -0400

    Hit MVP but am going to refactor a bit.

--- src/components/FollowerList.js
+++ src/components/FollowerList.js
@@ -1,11 +1,18 @@
 import React from "react";
 import UserCard from "./UserCard";
 const FollowerList = (props) => {
-  //   console.log("FollowerList", props.followers);
+  console.log("FollowerList", props.followers);
   return (
     <div classname="followerlist">
       {props.followers.map((follower) => (
-        <UserCard />
+        <UserCard
+          name={follower.name}
+          login={follower.login}
+          image={follower.avatar_url}
+          location={follower.location || "Unknown"}
+          followers={follower.followers || "Unknown"}
+          following={follower.following || "Unknown"}
+        />
       ))}
     </div>
   );
--- src/components/userCard.js
+++ src/components/userCard.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const UserCard = (props) => {
+  console.log("UserCar props", props);
   return (
     <div>
-      <h1>{props.name}</h1>
+      <h1>{props.name || props.login}</h1>
       <img src={props.image} />
       <p>Location: {props.location}</p>
       <p>Followers: {props.followers}</p>
